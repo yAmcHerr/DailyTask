abb82f86755e20bea3d3a68c537eed6c046cf09d
yAmcHerr <167756706+yAmcHerr@users.noreply.github.com>
2026-09-04 14:21:13 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'todo-tracker-v6';
+const CACHE_NAME = 'todo-tracker-v8';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon.svg', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
