1d03faf966e1f9506577fbbcd034325f9b8327e2
yAmcHerr <167756706+yAmcHerr@users.noreply.github.com>
2026-09-04 16:31:33 +0800

    Update CACHE_NAME to version 2.3.4.5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'todo-tracker-v8';
+const CACHE_NAME = 'todo-tracker-v2.3.4.5';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon.svg', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
